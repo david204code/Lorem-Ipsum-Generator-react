80c0d36de0c6b195e1e55ff74c2673625898d32e
David Wu <david204code@gmail.com>
2021-01-12 23:42:57 +0000

    iterating over the data with the map method

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Praise the Lord!")
+
+    setText(data);
   }
 
   return (
@@ -27,12 +28,12 @@ function App() {
         <button type ="submit" className ="btn">Generate</button>
       </form>
       <article className ="lorem-text">
-        <p>
-          Aute ea non minim duis elit proident proident ullamco. Magna qui dolor commodo sit dolore labore tempor labore do velit ut est. Enim duis id cillum fugiat qui officia commodo. Elit eiusmod minim amet consequat aliquip deserunt dolore sit esse nulla laboris laboris est. Ex exercitation in anim ea elit Lorem est mollit adipisicing non cillum id. 
-        </p>
-        <p>
-          Aute ea non minim duis elit proident proident ullamco. Magna qui dolor commodo sit dolore labore tempor labore do velit ut est. Enim duis id cillum fugiat qui officia commodo. Elit eiusmod minim amet consequat aliquip deserunt dolore sit esse nulla laboris laboris est. Ex exercitation in anim ea elit Lorem est mollit adipisicing non cillum id. 
-        </p>
+        {/* iterate over the text array */}
+        {text.map((item, index) => {
+          return(
+            <p key ={index}>{item}</p>
+          );
+        })}
       </article>
     </section>
   );
